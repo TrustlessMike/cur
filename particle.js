edbb9da8dd1ea0a0ac2d7e69320ca36c2d0442c3
TrustlessMike <trustless.mike@proton.me>
2024-08-30 14:47:54 -0400

    Empty change

--- particle.js
+++ particle.js
@@ -31,4 +31,12 @@ function animate() {
     renderer.render(scene, camera);
 }
 
+window.addEventListener('resize', onWindowResize, false);
+
+function onWindowResize() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+}
+
 init();
